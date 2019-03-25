5cfac1cd4d6fac0bdca7214fb56d002cca382bf6
Dong Liu <doli@kth.se>
2019-03-25 18:24:49 +0100

    including powerEP and expansion

--- ep.py
+++ ep.py
@@ -3,30 +3,54 @@ import matplotlib
 import matplotlib.pyplot as plt
 import itertools
 from scipy.stats import multivariate_normal
-from modules import GaussianDiag, EP, MMSE
+from modules import GaussianDiag, EP, MMSE, PowerEP, StochasticEP, ExpansionEP, ExpansionPowerEP
 from utils import channel_component, sampling_noise, sampling_signal, sampling_H,real2complex
 # configuretion
 class hparam(object):
     num_tx = 4
     num_rx = 4
-    soucrce_prior = [0.2, 0.8]
+    soucrce_prior = [0.5, 0.5]
     signal_var = 1
     snr = np.linspace(1,32,32)
     monte = 5000
-    ep_iteration = 16
+    ep_iteration = 10
+    power_n = 4./3
     constellation = [int(-1), int(1)]
+
+    
+    #algos_list = ["MMSE", "EP", "PowerEP"]
+    algos = {"MMSE": {"detector": MMSE},
+             "EP": {"detector": EP},
+             "ExpansionEP": {"detector": ExpansionEP},
+             "ExpansionPowerEP": {"detector": ExpansionPowerEP}
+    }
+    for _, value in algos.items():
+        value["ser"] = []
+    
+    
+
 
 ### begin test ########
 
-mmse = MMSE()
-ser_mmse = []
-ser_ep = []
-ser_ep_map = []
+# mmse = MMSE()
+# ser_mmse = []
+# ser_ep = []
+# ser_ep_map = []
+# ser_power_ep = []
+# ser_power_ep_map =[]
+# ser_sep = []
+
 
 for snr in hparam.snr:
-    ser_mmse_tmp = []
-    ser_ep_tmp = []
-    ser_ep_map_tmp = []
+    tmp = dict()
+    for name,_ in hparam.algos.items():
+        tmp[name] = []
+    # ser_mmse_tmp = []
+    # ser_ep_tmp = []
+    # ser_ep_map_tmp = []
+    # ser_power_ep_tmp = []
+    # ser_power_ep_map_tmp = []
+    # ser_sep_tmp = []
 
     for monte in range(hparam.monte):
         x, true_symbol = sampling_signal(hparam)
@@ -34,36 +58,94 @@ for snr in hparam.snr:
         noise, noise_var = sampling_noise(hparam=hparam, snr=snr)
         channel = sampling_H(hparam)
         noised_signal = np.dot(channel,x) + noise
+        for key, method in hparam.algos.items():
+            if key is "MMSE":
+                #### mes detection
+                detector = method["detector"]()
+                power_ratio = noise_var/hparam.signal_var
+                estimated_symbol = detector.detect(y=noised_signal, channel=channel, power_ratio=power_ratio)
+                #estimated_symbol = real2complex(np.sign(detected_by_mmse))
+            else:
+                detector = method['detector'](noise_var, hparam)
+                detector.fit(channel=channel,
+                          noise_var=noise_var,
+                          noised_signal=noised_signal,
+                          stop_iter=hparam.ep_iteration)
+                estimated_symbol = detector.detect_signal_by_mean()
+
+
+
+            est_complex_symbol = real2complex(estimated_symbol)
+            error = np.sum(true_symbol != est_complex_symbol)
+            
+            tmp[key].append(error)
+
+        #### ep detection and performance collections
+        # my_ep = EP(noise_var, hparam)
         
-        #### mse detection
-        power_ratio = noise_var/hparam.signal_var
-        detected_by_mmse = mmse.detect(y=noised_signal, channel=channel, power_ratio=power_ratio)
-        mmse_symbol = real2complex(np.sign(detected_by_mmse))
-        error_mmse = np.sum(true_symbol != mmse_symbol)
-        ser_mmse_tmp.append(error_mmse)
+        # my_ep.fit(channel=channel,
+        #           noise_var=noise_var,
+        #           noised_signal=noised_signal,
+        #           stop_iter=hparam.ep_iteration)
+        # ep_symbol = real2complex(my_ep.detect_signal_by_mean())
+        # ser_ep_tmp.append( np.sum(true_symbol != ep_symbol) )
 
-        #### ep detection
-        my_ep = EP(noise_var, hparam)
-        for i in range(2):
-            my_ep.iteration(channel=channel,
-                            noise_var=noise_var,
-                            noised_signal=noised_signal)
-        ep_symbol = real2complex(my_ep.detect_signal_by_mean())
-        ser_ep_tmp.append( np.sum(true_symbol != ep_symbol) )
-
-        ep_map_symbol = real2complex( my_ep.detect_signal_by_map() )
-        ser_ep_map_tmp.append( np.sum(true_symbol != ep_map_symbol) )
-
+        # ep_map_symbol = real2complex( my_ep.detect_signal_by_map() )
+        # ser_ep_map_tmp.append( np.sum(true_symbol != ep_map_symbol) )
+        
+        #### Stochastic EP detection and performance collections
+        # my_sep = StochasticEP(noise_var, hparam)
+        # my_sep.fit(channel=channel,
+        #           noise_var=noise_var,
+        #           noised_signal=noised_signal,
+        #           stop_iter=hparam.ep_iteration)
+        # sep_symbol = real2complex( my_sep.detect_signal_by_mean() )
+        # ser_sep_tmp.append( np.sum(true_symbol != sep_symbol) )
+        
+        ##### PowerEP detection and performance collections
+        # power_EP = PowerEP(noise_var=noise_var,
+        #                    hparam=hparam,
+        #                    power_n=2)
+        # power_EP.fit(channel=channel,
+        #              noise_var=noise_var,
+        #              noised_signal=noised_signal,
+        #              stop_iter=hparam.ep_iteration)
+        ## 1st moment detection
+        # power_ep_symbol = real2complex(power_EP.detect_signal_by_mean())
+        # ser_power_ep_tmp.append( np.sum(true_symbol != power_ep_symbol))
+        ## MAP detection
+        # power_ep_map_symbol = real2complex( power_EP.detect_signal_by_map() )
+        # ser_power_ep_map_tmp.append( np.sum(true_symbol != power_ep_map_symbol))
+        
+    for key, method in hparam.algos.items():
+        method["ser"].append( np.mean(tmp[key])/hparam.num_tx )
+    # ser_mmse.append( np.mean(ser_mmse_tmp)/hparam.num_tx )
+    # ser_ep.append( np.mean(ser_ep_tmp)/hparam.num_tx )
+    #ser_ep_map.append( np.mean(ser_ep_map_tmp)/hparam.num_tx )
     
-    ser_mmse.append( np.mean(ser_mmse_tmp)/hparam.num_tx )
-    ser_ep.append( np.mean(ser_ep_tmp)/hparam.num_tx )
-    ser_ep_map.append( np.mean(ser_ep_map_tmp)/hparam.num_tx )
-
+    ## collection performance of PowerEP of moment detection
+    # ser_power_ep.append( np.mean(ser_power_ep_tmp)/hparam.num_tx )
+    # ser_power_ep_map.append( np.mean(ser_power_ep_map_tmp)/hparam.num_tx )
     
+    ## collection performance of StochasticEP
+    # ser_sep.append( np.mean(ser_sep_tmp)/hparam.num_tx )
+
+marker_list = ["o", "<", "+", ">"]
+iter_marker_list = iter(marker_list)
 fig, ax = plt.subplots()
-ax.semilogy(hparam.snr, ser_mmse, label = "MMSE")
-ax.semilogy(hparam.snr, ser_ep, label = "EP_mean")
-ax.semilogy(hparam.snr, ser_ep_map, label = "EP_MAP")
+for key, method in hparam.algos.items():
+    ax.semilogy(hparam.snr, method["ser"],
+                label = key + "_Iteration:{}".format(hparam.ep_iteration) if "MMSE" not in key else "",
+                marker=next(iter_marker_list))
+    
+# ax.semilogy(hparam.snr, ser_mmse, label = "MMSE")
+# ax.semilogy(hparam.snr, ser_ep, label = "EP_mean", marker="o")
+# # ax.semilogy(hparam.snr, ser_ep_map, label = "EP_MAP", marker="<")
+# ax.semilogy(hparam.snr, ser_power_ep, label = "PowerEP_mean", marker="+")
+# ax.semilogy(hparam.snr, ser_power_ep_map, label = "PowerEP_map", marker=">")
+
+# ax.semilogy(hparam.snr, ser_sep, label = "SEP", marker = "1")
+
 ax.legend()
 ax.set(xlabel="SNR", ylabel="SER")
 ax.grid()
--- modules.py
+++ modules.py
@@ -22,10 +22,11 @@ class GaussianDiag:
         return likelihood
 
 class EP(object):
+    
     def __init__(self, noise_var, hparam):
         #### two intermediate parameters needed to update
         self.gamma = np.zeros(hparam.num_tx*2)
-        self.Sigma = np.ones(hparam.num_tx*2) / hparam.signal_var
+        self.Sigma =  np.ones(hparam.num_tx*2) / hparam.signal_var
         #### two parameters for q(u)
         self.mu = np.zeros_like(self.gamma)
         self.covariance = np.diag(np.ones_like(self.gamma))
@@ -40,38 +41,43 @@ class EP(object):
 
     def get_moments(self):
         return (self.mu, self.covariance)
-         
+    
 
     def update_moments(self, channel, noise_var, noised_signal):
         noised_signal = np.array(noised_signal)
         self.covariance = np.linalg.inv(np.matmul(channel.T, channel)/noise_var
-                                            + np.diag(self.Sigma))
+                                        + np.diag(self.Sigma))
         tmp = channel.T.dot(noised_signal)/noise_var
         self.mu = np.dot(self.covariance,
-                          tmp+ self.gamma )
+                         tmp+ self.gamma )
         #assert np.all(self.covariance>=0)
 
     def update_cavity(self):
         for i in range(self.mu.shape[0]):
             self.cavity_mgnl_h[i] = self.covariance[i, i]/( 1 - self.covariance[i, i] * self.Sigma[i])
             self.cavity_mgnl_t[i] = self.cavity_mgnl_h[i] * (self.mu[i]/self.covariance[i,i] - self.gamma[i])
-        assert np.all(self.cavity_mgnl_h>=0)
+            assert np.all(self.cavity_mgnl_h>=0)
 
 
     def update_prox_moments(self):
-        gaussian = GaussianDiag()
+        vary_small = 1e-6
+        #gaussian = GaussianDiag()
         mean = self.cavity_mgnl_t
-        logs = np.log(self.cavity_mgnl_h)/2
+        #logs = np.log(self.cavity_mgnl_h)/2
         z = None
         for i, the_mean in enumerate(mean):
-            logp = gaussian.likelihood(mean=the_mean,
-                                       logs=logs[i],
-                                       x= np.array(self.constellation))
+            # logp = gaussian.likelihood(mean=the_mean,
+            #                            logs=logs[i],
+            #                            x= np.array(self.constellation))
+            logp = np.log(multivariate_normal.pdf(x=np.array(self.constellation),
+                                                  mean=the_mean,
+                                                  cov=self.cavity_mgnl_h[i]) + vary_small )
             z = np.sum(np.exp(logp))
             self.prox_mu[i] = np.array(self.constellation).dot( np.exp(logp) )/z
-            second_moment = np.power(np.array(self.constellation), 2).dot( np.exp(logp) )/z
-            self.prox_var[i] = second_moment -  np.power(self.prox_mu[i], 2)
-        assert np.all(self.prox_var>=0)
+            # second_moment = np.power(np.array(self.constellation), 2).dot( np.exp(logp) )/z
+            # self.prox_var[i] = second_moment -  np.power(self.prox_mu[i], 2)
+            self.prox_var[i] = np.power(np.array(self.constellation) - self.prox_mu[i], 2).dot( np.exp(logp) )/z
+            assert np.all(self.prox_var>=0)
 
     def kl_match_momoents(self):
         vary_small = 1e-6
@@ -83,16 +89,17 @@ class EP(object):
             positive_idx = Sigma>0
             self.Sigma[positive_idx] = Sigma[positive_idx]
             self.gamma[positive_idx] = gamma[positive_idx]
-        assert np.all(self.Sigma>0)
+            assert np.all(self.Sigma>0)
 
-    def iteration(self, channel, noise_var, noised_signal):
-        
-        self.update_moments(channel=channel,
-                            noise_var=noise_var,
-                            noised_signal=noised_signal)
-        self.update_cavity()
-        self.update_prox_moments()
-        self.kl_match_momoents()
+    def fit(self, channel, noise_var, noised_signal, stop_iter=10):
+        """Do the training by number of iteration of stop_iter"""
+        for i in range(stop_iter):
+            self.update_moments(channel=channel,
+                                noise_var=noise_var,
+                                noised_signal=noised_signal)
+            self.update_cavity()
+            self.update_prox_moments()
+            self.kl_match_momoents()
 
 
     def detect_signal_by_mean(self):
@@ -116,7 +123,87 @@ class EP(object):
         #     logp_list.append(logp)
         idx_max = np.argmax( p_list )
         return proposals[idx_max]
-            
+
+class PowerEP(EP):
+    
+    def __init__(self, noise_var, hparam, power_n=1):
+        super().__init__(noise_var, hparam)
+        #### set the power n for PowerEP, integer
+        self.power_n = hparam.power_n
+        #assert self.power_n > 0
+
+    def update_cavity(self):
+        for i in range(self.mu.shape[0]):
+            self.cavity_mgnl_h[i] = self.covariance[i, i]/( 1 - self.covariance[i, i] * self.Sigma[i] / self.power_n)
+            self.cavity_mgnl_t[i] = self.cavity_mgnl_h[i] * (self.mu[i]/self.covariance[i,i] - self.gamma[i] / self.power_n)
+
+        assert np.all(self.cavity_mgnl_h>=0)
+        
+    def kl_match_momoents(self):
+        vary_small = 1e-6
+        Sigma = self.power_n * (1./(self.prox_var + vary_small) - 1./(self.cavity_mgnl_h + vary_small))
+        gamma = self.power_n * (self.prox_mu / (self.prox_var+vary_small) - self.cavity_mgnl_t / (self.cavity_mgnl_h+vary_small))
+        if np.any(np.isnan(Sigma)) and np.any(np.isnan(gamma)):
+            print("value error")
+        if np.any(Sigma>0):
+            positive_idx = Sigma>0
+            self.Sigma[positive_idx] = Sigma[positive_idx]
+            self.gamma[positive_idx] = gamma[positive_idx]
+        assert np.all(self.Sigma>0)
+
+### definition of stochastic EP:
+class StochasticEP(EP):
+    def __init__(self, noise_var, hparam):
+        super().__init__(noise_var, hparam)
+
+    def kl_match_momoents(self):
+        """Moment matching step"""
+        vary_small = 1e-6
+        Sigma = 1./(self.prox_var + vary_small) - 1./(self.cavity_mgnl_h + vary_small)
+        gamma = self.prox_mu / (self.prox_var+vary_small) - self.cavity_mgnl_t / (self.cavity_mgnl_h+vary_small)
+        if np.any(np.isnan(Sigma)) and np.any(np.isnan(gamma)):
+            print("value error")
+        if np.any(Sigma>0):
+            positive_idx = Sigma>0
+            n = np.sum(positive_idx)
+            total_n = self.Sigma.size
+            self.Sigma = ( self.Sigma[0] * (1 - n / total_n) +  np.sum(Sigma[positive_idx]) * ( 1 / total_n) ) * np.ones_like(self.Sigma)
+            self.gamma = (self.gamma[0] * ( 1 - n / total_n) +  np.sum(gamma[positive_idx]) * ( 1 / total_n) ) * np.ones_like(self.gamma)
+            assert np.all(self.Sigma>0)
+    
+
+class ExpansionEP(EP):
+    """Do the improvement of EP, using the basic expansion: the p(x) is appx as 
+    \sum_n q_n(x) - (N-1) p(x), q_n is the n-th title distribution"""
+    
+    def __init__(self, noise_var, hparam):
+        super().__init__(noise_var, hparam)
+        
+    def detect_signal_by_mean(self):
+        """1st order correction"""
+        estimated_signal = []
+        
+        for mu in self.prox_mu:
+            obj_list  = np.abs(mu - np.array(self.constellation))
+            estimated_signal.append(self.constellation[np.argmin(obj_list)])
+        return estimated_signal
+
+class ExpansionPowerEP(PowerEP):
+    """Do the improvement of EP, using the basic expansion: the p(x) is appx as 
+    \sum_n q_n(x) - (N-1) p(x), q_n is the n-th title distribution"""
+    
+    def __init__(self, noise_var, hparam):
+        super().__init__(noise_var, hparam)
+        
+    def detect_signal_by_mean(self):
+        """1st order correction to power EP"""
+        
+        estimated_signal = []
+        
+        for mu in self.prox_mu:
+            obj_list  = np.abs(mu - np.array(self.constellation))
+            estimated_signal.append(self.constellation[np.argmin(obj_list)])
+        return estimated_signal
 
 
 class MMSE(object):
@@ -126,5 +213,5 @@ class MMSE(object):
         inv = np.linalg.inv(power_ratio * np.eye(channel.shape[1]) 
                             + np.matmul(channel.T, channel) )
         x = inv.dot(channel.T).dot(y)
-        return x
+        return np.sign(x)
 
