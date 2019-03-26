027864df6f2714f22c7ac60062144ea9a3b3178d
Dong Liu <doli@sim410.ee.kth.se>
2019-03-26 09:18:36 +0100

    parallel_compute

--- ep.py
+++ ep.py
@@ -2,17 +2,21 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import itertools
+from collections import defaultdict
+
+from joblib import Parallel, delayed
 from scipy.stats import multivariate_normal
 from modules import GaussianDiag, EP, MMSE, PowerEP, StochasticEP, ExpansionEP, ExpansionPowerEP
 from utils import channel_component, sampling_noise, sampling_signal, sampling_H,real2complex
+
 # configuretion
 class hparam(object):
     num_tx = 4
     num_rx = 4
     soucrce_prior = [0.5, 0.5]
     signal_var = 1
-    snr = np.linspace(1,32,32)
-    monte = 5000
+    snr = np.linspace(1,30,60)
+    monte = 50000
     ep_iteration = 10
     power_n = 4./3
     constellation = [int(-1), int(1)]
@@ -26,31 +30,12 @@ class hparam(object):
     }
     for _, value in algos.items():
         value["ser"] = []
-    
-    
 
+def worker(snr):
 
-### begin test ########
-
-# mmse = MMSE()
-# ser_mmse = []
-# ser_ep = []
-# ser_ep_map = []
-# ser_power_ep = []
-# ser_power_ep_map =[]
-# ser_sep = []
-
-
-for snr in hparam.snr:
     tmp = dict()
     for name,_ in hparam.algos.items():
         tmp[name] = []
-    # ser_mmse_tmp = []
-    # ser_ep_tmp = []
-    # ser_ep_map_tmp = []
-    # ser_power_ep_tmp = []
-    # ser_power_ep_map_tmp = []
-    # ser_sep_tmp = []
 
     for monte in range(hparam.monte):
         x, true_symbol = sampling_signal(hparam)
@@ -80,77 +65,39 @@ for snr in hparam.snr:
             
             tmp[key].append(error)
 
-        #### ep detection and performance collections
-        # my_ep = EP(noise_var, hparam)
-        
-        # my_ep.fit(channel=channel,
-        #           noise_var=noise_var,
-        #           noised_signal=noised_signal,
-        #           stop_iter=hparam.ep_iteration)
-        # ep_symbol = real2complex(my_ep.detect_signal_by_mean())
-        # ser_ep_tmp.append( np.sum(true_symbol != ep_symbol) )
-
-        # ep_map_symbol = real2complex( my_ep.detect_signal_by_map() )
-        # ser_ep_map_tmp.append( np.sum(true_symbol != ep_map_symbol) )
-        
-        #### Stochastic EP detection and performance collections
-        # my_sep = StochasticEP(noise_var, hparam)
-        # my_sep.fit(channel=channel,
-        #           noise_var=noise_var,
-        #           noised_signal=noised_signal,
-        #           stop_iter=hparam.ep_iteration)
-        # sep_symbol = real2complex( my_sep.detect_signal_by_mean() )
-        # ser_sep_tmp.append( np.sum(true_symbol != sep_symbol) )
-        
-        ##### PowerEP detection and performance collections
-        # power_EP = PowerEP(noise_var=noise_var,
-        #                    hparam=hparam,
-        #                    power_n=2)
-        # power_EP.fit(channel=channel,
-        #              noise_var=noise_var,
-        #              noised_signal=noised_signal,
-        #              stop_iter=hparam.ep_iteration)
-        ## 1st moment detection
-        # power_ep_symbol = real2complex(power_EP.detect_signal_by_mean())
-        # ser_power_ep_tmp.append( np.sum(true_symbol != power_ep_symbol))
-        ## MAP detection
-        # power_ep_map_symbol = real2complex( power_EP.detect_signal_by_map() )
-        # ser_power_ep_map_tmp.append( np.sum(true_symbol != power_ep_map_symbol))
-        
+    performance = {"snr": snr}
     for key, method in hparam.algos.items():
-        method["ser"].append( np.mean(tmp[key])/hparam.num_tx )
-    # ser_mmse.append( np.mean(ser_mmse_tmp)/hparam.num_tx )
-    # ser_ep.append( np.mean(ser_ep_tmp)/hparam.num_tx )
-    #ser_ep_map.append( np.mean(ser_ep_map_tmp)/hparam.num_tx )
+        #method["ser"].append( np.mean(tmp[key])/hparam.num_tx )
+        performance[key] =  np.mean(tmp[key])/hparam.num_tx 
+    return performance
+
+RESULTS = Parallel(n_jobs=10, pre_dispatch="all", backend="threading")(map(delayed(worker), list(hparam.snr)))
+
+performance = defaultdict(list)
+for key, _ in hparam.algos.items():
+    for the_result in RESULTS:
+        performance[key].append( the_result[key] )
+
     
-    ## collection performance of PowerEP of moment detection
-    # ser_power_ep.append( np.mean(ser_power_ep_tmp)/hparam.num_tx )
-    # ser_power_ep_map.append( np.mean(ser_power_ep_map_tmp)/hparam.num_tx )
     
-    ## collection performance of StochasticEP
-    # ser_sep.append( np.mean(ser_sep_tmp)/hparam.num_tx )
+    
+# for snr in hparam.snr:
+
 
 marker_list = ["o", "<", "+", ">"]
 iter_marker_list = iter(marker_list)
 fig, ax = plt.subplots()
 for key, method in hparam.algos.items():
-    ax.semilogy(hparam.snr, method["ser"],
-                label = key + "_Iteration:{}".format(hparam.ep_iteration) if "MMSE" not in key else "",
+    ax.semilogy(hparam.snr, performance[key],
+                label = key + "_Iteration:{}".format(hparam.ep_iteration) if "MMSE" not in key else "MMSE",
                 marker=next(iter_marker_list))
     
-# ax.semilogy(hparam.snr, ser_mmse, label = "MMSE")
-# ax.semilogy(hparam.snr, ser_ep, label = "EP_mean", marker="o")
-# # ax.semilogy(hparam.snr, ser_ep_map, label = "EP_MAP", marker="<")
-# ax.semilogy(hparam.snr, ser_power_ep, label = "PowerEP_mean", marker="+")
-# ax.semilogy(hparam.snr, ser_power_ep_map, label = "PowerEP_map", marker=">")
-
-# ax.semilogy(hparam.snr, ser_sep, label = "SEP", marker = "1")
 
 ax.legend()
 ax.set(xlabel="SNR", ylabel="SER")
 ax.grid()
-#fig.savefig("mmse_ep_SER_prior{}.pdf".format(hparam.soucrce_prior))
-plt.show()
+fig.savefig("figures/experiments_3-25.pdf")
+#plt.show()
 
         
         
