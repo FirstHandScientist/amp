3826bb31dbf396add58b44fb57295a150271c60d
Dong Liu <doli@kth.se>
2019-09-02 15:21:05 +0200

    fix import path for ep

--- bin/ep.py
+++ bin/ep.py
@@ -12,6 +12,9 @@ from collections import defaultdict
 import pickle
 from joblib import Parallel, delayed
 from scipy.stats import multivariate_normal
+import sys
+sys.path.append("./src")
+
 from modules import GaussianDiag, EP, MMSE, PowerEP, StochasticEP, ExpansionEP, ExpansionPowerEP, ExpectationConsistency, LoopyBP, LoopyMP, PPBP, AlphaBP, MMSEalphaBP, ML, VariationalBP, MMSEvarBP, EPalphaBP, StochasticBP
 from utils import channel_component, sampling_noise, sampling_signal, sampling_H,real2complex
 
